f84c8e2a535ab66e70066f73e21717acc205f3c3
Tom Hummel <tphummel@gmail.com>
2014-05-14 07:26:08 -0700

    module

---
+++ index.js
@@ -0,0 +1,31 @@
+function Increment (opts) {
+  opts = opts ? opts : {};
+
+  this.radix = opts.radix || 10;
+  this.start = opts.start || 0;
+
+  this.position = this.start;
+
+  return this;
+}
+
+Increment.prototype.next = function(){
+  this.position += 1;
+
+  if(this.radix !== 10){
+    return this.position.toString(this.radix);
+  }else{
+    return this.position;
+  }
+}
+
+Increment.prototype.reset = function(){
+  if(this.position > 0){
+    this.position = 0;
+    return true;
+  }else{
+    return false;
+  }
+}
+
+module.exports = Increment;
